1a889b8db68fc7f8f12cd917c00ffd86b6d97b22
Mohamed Amine EL GNAOUI <mohamed.amine.elgnaoui@gmail.com>
2018-06-26 16:13:15 +0200

    Implementing wbuff to write on buffers

--- rootio/wbuffer.go
+++ rootio/wbuffer.go
@@ -4,6 +4,45 @@
 
 package rootio
 
+import(
+	"io"
+)
+type wbuff struct {
+	p []byte // buffer of data to write on
+	c int    // current position in buffer of data
+}
+
+func (w *wbuff) Write(p []byte) (int, error) {
+	if w.c >= len(w.p) {
+		return 0, io.EOF
+	}
+	n := copy(w.p[w.c:], p)
+	w.c += n
+	return n, nil
+}
+
+func (w *wbuff) WriteByte(p byte) error {
+	if w.c >= len(w.p) {
+		return io.EOF
+	}
+	w.p[w.c] = p
+	w.c++
+	return nil
+}
+
+
+
+
 // WBuffer is a write-only ROOT buffer for streaming.
 type WBuffer struct {
+	w      *wbuff
+	err    error
+	offset uint32
+	refs   map[int64]interface{}
+	sictx  StreamerInfoContext
 }
+
+
+
+
+
